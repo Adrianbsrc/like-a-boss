a16b2ee1e894278ffe9f63a83f4ffcfc71fde783
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-07-04 02:19:21 -0300

    gulp setting

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,20 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function(){
-  return gulp.src('src/scss/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('public/css'))  
-});
+var scssFiles = './src/scss/**/*.scss';
+var cssFolder = './public/css/';
+
+gulp.task('style', function(){
+  gulp.src(scssFiles)
+    .pipe(sass({
+      errLogToConsole: true,
+      outputStyle: 'compressed'
+    })).on('error', console.error.bind(console))
+    .pipe(rename({suffix:'.min'}))
+    .pipe(gulp.dest(cssFolder))
+})
+
+gulp.task('watch', function(){
+  gulp.watch([scssFiles], gulp.series('style'))
+})
