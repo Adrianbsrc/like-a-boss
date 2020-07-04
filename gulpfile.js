db6945cd5fd3e59a928d40b1763daff9c8cc82b5
Thadeu Cesário <thadeu.cesario@icloud.com>
2020-07-04 03:05:00 -0300

    gulp - settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
+var uglifycss = require('gulp-uglifycss');
 
 var scssFiles = './src/scss/**/*.scss';
 var cssFolder = './public/css/';
@@ -13,8 +14,21 @@ gulp.task('style', function(){
     })).on('error', console.error.bind(console))
     .pipe(rename({suffix:'.min'}))
     .pipe(gulp.dest(cssFolder))
+});
+
+gulp.task('css', function(){
+  gulp.src('./public/css/*.css')
+  .pipe(uglifycss({
+    "uglyComments": true
+  }))
+  .pipe(gulp.dest(cssFolder))
+
+  gulp.series('watch');
 })
 
 gulp.task('watch', function(){
   gulp.watch([scssFiles], gulp.series('style'))
-})
+  gulp.watch([scssFiles], gulp.series('css'))
+});
+
+gulp.task('default', gulp.parallel('style','css','watch'))
